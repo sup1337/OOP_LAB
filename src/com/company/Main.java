1c5a6ccddc399c7e708bdc465e29898ede6e7b99
sup1337 <64952429+sup1337@users.noreply.github.com>
2021-02-22 01:22:09 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,54 @@
 package com.company;
 
 public class Main{
+    public static double maxElement( double array[] ){
+        double max = Double.NEGATIVE_INFINITY;
+        for( int i=0; i<array.length; ++i ){
+            if( array[i] > max){
+                max = array[ i ];
+            }
+        }
+        return max;
+    }
+    public static void getBit(int number, int order){
+        if(order < 0){
+            System.out.println(1);
+        }
+        else
+            System.out.println(((number & (1 << (order - 1))) >> (order - 1)));
+    }
 
-    public static void main(String[]args){
-        String word = "ALMAFA";
+
+    public static void main(String[]args) {
+        String szo = "ALMAFA";
         String name = "Laszlo Hunor";
         String mono = "L" + "H";
-        for(int i = 0 ; i<name.length(); i++){
+        for (int i = 0; i < name.length(); i++) {
             System.out.println(name.charAt(i));
         }
         System.out.println(mono);
-        for (int i = 0; i < word.length(); i++) {
-            for (int j = 0; j<=i; j++) {
-                System.out.print(word.charAt(i));
+        for (int i = 0; i < szo.length(); i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(szo.charAt(i));
             }
             System.out.println();
         }
-        String string = "Laszlo Benedek Kovacs";
-        String[] parts = string.split(" ");
-        System.out.println(string);
+        String names ="Laszlo Hunor Miklos Akos";
+        if (names.length() == 0)
+            return;
+        //split the string using 'space'
+        //and print the first character of every word
+        String words[] = names.split(" ");
+        for(String word : words) {
+            System.out.print(Character.toUpperCase(word.charAt(0)) + " ");
+        }
+        System.out.println();
+        double x[] ={7, 1, -3, 45, 9};
+        System.out.printf("MAX: %6.2f\n", maxElement( x ));
+
+        getBit(13,1);
+
+
 
     }
 }
