cb01462c51153d029dd54a0eed59643312c16cd3
sup1337 <64952429+sup1337@users.noreply.github.com>
2021-03-10 09:46:30 +0200

    1

--- 4/src/lab4_1/Main.java
+++ 4/src/lab4_1/Main.java
@@ -1,25 +1,42 @@
 package lab4_1;
 
-import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args)
+    {
 
-    public static void main(String[] args) {
-
-///
-
-
-
-
+        readFilePrintItsLineNumbered(("lab4_1_input.txt"));
+        readFilePrintItsLineNumbered(("lab4_1_input.csv"));
     }
 
+
     public static void readFilePrintItsLineNumbered( String fileName ){
+// Open the file
         Scanner scanner = null;
         try {
+
             scanner = new Scanner( new File(fileName));
+            //vagy itt
+            for(int i=0; scanner.hasNextLine(); i++)
+            {
+                String line =scanner.nextLine();
+                System.out.println(i + " " +line);
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        //vagy itt
+        //if(scanner != null)
+        // for(int i=0; scanner.hasNextLine(); i++)
+        //  {
+        //     String line =scanner.nextLine();
+        //     System.out.println(i + " " +line);
+        // }
+
+//...
     }
+
 }
--- 4/src/lab4_1/Person.java
+++ 4/src/lab4_1/Person.java
@@ -1,5 +1,46 @@
 package lab4_1;
 
 public class Person {
-    //ghdhgdhgdhfd
+    private String firstName;
+    private String lastName;
+    private int birthYear;
+
+    public Person(String firstName, String lastName, int birthYear) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthYear = birthYear;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthYear=" + birthYear +
+                '}';
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setBirthYear(int birthYear) {
+        this.birthYear = birthYear;
+    }
 }
